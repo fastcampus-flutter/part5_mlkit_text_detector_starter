ba02bf41649fa40e1357c8895db3affbcd8477c9
Dreamwalker <aristojeff@gmail.com>
2025-05-31 21:24:49 +0900

    patch: 2025-05-31

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # part5_mlkit_text_detector_starter
 
+# 변경사항
+## 2025-05-31
+1. 프로젝트 의존성 최신화
+2. 빌드시 win32 오류 관련 수정
+3. android 폴더 최신화
+
 "A new Flutter project."
--- android/app/src/main/kotlin/com/fastcampus/part5_mlkit_text_detector_starter/MainActivity.kt
+++ android/app/src/main/kotlin/com/fastcampus/part5_mlkit_text_detector_starter/MainActivity.kt
@@ -2,5 +2,4 @@ package com.fastcampus.part5_mlkit_text_detector_starter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
